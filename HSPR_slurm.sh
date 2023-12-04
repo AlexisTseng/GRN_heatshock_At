f28c69e98799b99b84114c8f2330e501b415f71c
AlexisTseng <alexiszeng@outlook.com>
2023-12-04 14:34:30 +0000

    changed output dir

--- HSPR_slurm.sh
+++ HSPR_slurm.sh
@@ -13,7 +13,7 @@
 #SBATCH --ntasks=1
 
 #SBATCH -D /home/jz531/rds/hpc-work/GRN_heatshock_At/
-#SBATCH -o ../Logs/hpc_run_63/parameter_scan_%a.log
+#SBATCH -o /home/jz531/rds/hpc-work/hpc_output/
 #SBATCH -c 1     # increase if doing multiprocessing, max 32 CPUs
 #SBATCH --array=1 # max is 9999
 #SBATCH --mem-per-cpu=5980MB   # max 5980MB or 12030MB for skilake-himem
