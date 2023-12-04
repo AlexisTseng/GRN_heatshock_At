1a2ead69d9b53b872c4de1d1ff55f17786fcf015
AlexisTseng <alexiszeng@outlook.com>
2023-12-04 15:39:12 +0000

    commented tips to make python script run

--- HSPR_slurm.sh
+++ HSPR_slurm.sh
@@ -28,10 +28,10 @@
 . /etc/profile.d/modules.sh # Leave this line (enables the module command)
 module purge  # Removes all modules still loaded
 module load miniconda/3
-source /home/jz531/.bashrc
+source /home/jz531/.bashrc # need to source before conda activate
 conda activate /home/jz531/.conda/envs/model_GRN
-conda list
+#conda list
 
-CMD="python3 HSPR_AZ_v6.py -nit 2 -tsp 20 -hss 10 -hsd 5"
+CMD="python3 HSPR_AZ_v6.py -nit 2 -tsp 20 -hss 10 -hsd 5" # need to use python3 instead of pythons
 
 eval $CMD
