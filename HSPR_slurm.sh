ff3ab0803ee6b334659882e57ee67d1849526223
AlexisTseng <alexiszeng@outlook.com>
2023-12-13 18:22:45 +0000

    called python from env directly

--- HSPR_slurm.sh
+++ HSPR_slurm.sh
@@ -27,10 +27,12 @@
  
 . /etc/profile.d/modules.sh # Leave this line (enables the module command)
 module purge  # Removes all modules still loaded
-#module load rhel7/default-ccl
+module load rhel7/default-ccl
+
+
 module load miniconda/3
 source /home/jz531/.bashrc # need to source before conda activate
-source activate /home/jz531/.conda/envs/model_GRN2
+conda activate /home/jz531/.conda/envs/model_GRN
 conda list
 CMD="python3 --version"
 #CMD="python3 HSPR_AZ_hpc.py -nit 1 -lkg 0.001" # need to use python3 instead of pythons
@@ -39,5 +41,5 @@ eval $CMD
 CMD="python --version"
 eval $CMD
 
-CMD="python3 HSPR_AZ_hpc.py -nit 1 -lkg 0.001" # need to use python3 instead of pythons
+CMD="/home/jz531/.conda/envs/model_GRN/bin/python HSPR_AZ_hpc.py -nit 1 -lkg 0.001" # need to use python3 instead of pythons
 eval $CMD
